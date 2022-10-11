bb79c09b7cd0331791ca81068bbf843a9fb08b03
theVoogie <vugar.abdulzada@webstep.no>
2022-10-11 17:02:30 +0200

    Added playwright function

--- functions/gen-opengraph-label/gen-opengraph-label.js
+++ functions/gen-opengraph-label/gen-opengraph-label.js
@@ -1,6 +1,40 @@
-exports.handler = async function(event, context) {
+const playwright = require("playwright-aws-lambda");
+
+exports.handler = async function(event, ctx) {
+  const browser = await playwright.launchChromium();
+  const context = await browser._defaultContext();
+  const page = await context.newPage();
+  await page.setContent(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+    </head>
+    <body>
+      <div> id="dexter">Hello</div>
+    </body>
+    </html>
+  `)
+  const bbox = await page.evaluate(() => {
+    const dexter = document.getElementById("dexter");
+    const { x, y, width, height } = dexter.children[0].getBoundingClientRect();
+    console.log('---> here you go: ', { x, y, width, height })
+    return { x, y, width, height };
+  });
+
+  const screenshotBuffer = await page.screenshot({ clip: bbox });
+  await browser.close();
+
   return {
+    isBase64Encoded: true,
     statusCode: 200,
-    body: "---> the generator function works fine"
+    headers: {
+      "Content-Type": "image/png",
+      "Content-Length": screenshotBuffer.length.toString()
+    },
+    body: screenshotBuffer.toString("base64")
   }
 };
