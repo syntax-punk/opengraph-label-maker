e58683faf95f88a2cb3533212d8bc648982bec4f
theVoogie <vugar.abdulzada@webstep.no>
2022-10-13 11:48:22 +0200

    Fix 56

---
+++ functions/gen-opengraph-label/build/precache-manifest.82de936c97077c5f4ac25a6d4beb94c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed79e234f847c5f5b541d4673581fe7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb73b601ee8fa6ffa49d",
+    "url": "/static/js/2.6e23d409.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.6e23d409.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "466fcae8888aa0d023b8",
+    "url": "/static/js/3.a64945e4.chunk.js"
+  },
+  {
+    "revision": "2390e5e2104ee8d55d36",
+    "url": "/static/js/main.0f06aef1.chunk.js"
+  },
+  {
+    "revision": "a589d37a34d2c4494ca4",
+    "url": "/static/js/runtime-main.33d144b0.js"
+  }
+]);
