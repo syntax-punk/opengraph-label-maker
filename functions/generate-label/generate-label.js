e4692614d41506096ec9b274149419ed31a86a93
theVoogie <vugar.abdulzada@webstep.no>
2022-10-14 13:44:48 +0200

    URL building

--- functions/generate-label/generate-label.js
+++ functions/generate-label/generate-label.js
@@ -1,10 +1,27 @@
 const playwright = require("playwright-aws-lambda");
 
+const getUrl = (queryStringParameters) => {
+  const {
+    cardpath = "https://earnest-youtiao-7b4d2a.netlify.app",
+    title = "No Title",
+    handle = "",
+    tags = "", 
+    width = 1200,
+    height = 630,
+  } = queryStringParameters;
+  return `${cardpath}?id=${id}&title=${title}&handle=${handle}&tags=${tags}&width=${width}&height=${height}`
+};
+
 exports.handler = async function(event, ctx) {
+  const { queryStringParameters } = event;
+  const url = getUrl(queryStringParameters)
+  console.log('-> query string: ', queryStringParameters)
+  console.log('-> url: ', url)
+  console.log('-> event: ', event)
   const browser = await playwright.launchChromium();
   const context = await browser.newContext();
   const page = await context.newPage();
-  await page.goto('https://earnest-youtiao-7b4d2a.netlify.app');
+  await page.goto(url);
 
   const bbox = await page.evaluate(() => {
     const root = document.getElementById("root");
