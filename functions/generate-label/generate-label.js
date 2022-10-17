feb0f720ab912292fad074aaf2f2b679ceaca58e
theVoogie <vugar.abdulzada@webstep.no>
2022-10-17 15:47:20 +0200

    Changed the url

--- functions/generate-label/generate-label.js
+++ functions/generate-label/generate-label.js
@@ -2,7 +2,7 @@ const playwright = require("playwright-aws-lambda");
 
 const getUrl = (queryStringParameters) => {
   const {
-    cardpath = "https://earnest-youtiao-7b4d2a.netlify.app",
+    cardpath = "https://nullpointer.no",
     title = "No Title",
     handle = "",
     tags = "", 
